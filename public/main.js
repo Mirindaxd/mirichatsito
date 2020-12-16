d3c21667c55d9202c2b85bfb1c180f96e1c0c0db
Miranda-Falcon <mirandafalconmiguelangel@gmail.com>
2020-12-16 00:32:43 -0600

    Chat heroku

---
+++ public/main.js
@@ -0,0 +1,32 @@
+var message;
+
+var socket = io.connect('http://localhost:8080', { 'forceNew': true });
+//Parte del cliente conectamos con localhost
+//escuchamos el evento messages
+// data tendr� el array de mensajes  que env�a el servidor
+socket.on('messages', function(data) {  
+  console.log(data);
+  render(data);
+})
+// esta funci�n se encarga de pintar en el HTML los mensajes
+function render (data) {  
+  var html = data.map(function(elem, index) {
+    return(`<div>
+              <strong>${elem.author}</strong>:
+              <em>${elem.text}</em>
+            </div>`);
+  }).join(" ");
+
+  document.getElementById('messages').innerHTML = html;
+}
+
+function addMessage(e) {  
+  
+   message = {
+    author: document.getElementById('username').value,
+    text: document.getElementById('texto').value
+  };
+
+  socket.emit('new-message', message);
+  return false;
+}
